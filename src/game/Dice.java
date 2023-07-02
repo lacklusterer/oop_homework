d5cc605265441183770170c3aa6cd5747657a440
minh210582 <minh.nn210582@sis.hust.edu.vn>
2023-07-02 22:30:14 +0700

    Implemented new id system using instance initializers

--- src/game/Dice.java
+++ src/game/Dice.java
@@ -5,30 +5,33 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class Dice {
-    private static int nextId = 1;
-    private int id;
+    private static int diceNumber = 0;
+    private int diceID;
     private Random random;
 
     ArrayList<Integer> sides = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6));
 
+    {
+        diceNumber++;
+        diceID = diceNumber;
+    }
+
     public Dice() {
-        this.id = nextId;
         this.random = new Random();
-        nextId++;
-        sides.remove(Integer.valueOf(id));
+        sides.remove(Integer.valueOf(diceID));
     }
 
     public int roll() {
         int chance = random.nextInt(100) + 1;
 
         if (chance <= 20) {
-            return id;
+            return diceID;
         } else {
             return sides.get(random.nextInt(5));
         }
     }
 
     public int getId() {
-        return id;
+        return diceID;
     }
 }
--- src/player/Player.java
+++ src/player/Player.java
@@ -3,8 +3,17 @@ package player;
 import game.Dice;
 
 public class Player {
+    private static int playerNumber = 0;
+    private int id;
+
     private String name;
     private int score;
+    private boolean bot;
+
+    {
+        playerNumber++;
+        id = playerNumber;
+    }
 
     public Player() {
         this.score = 0;
